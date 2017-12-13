5abbffc26dec5586af89d240c07d2d6048b14960
Constance Gobert <constancegobert@hotmail.fr>
2017-12-13 11:51:40 +0100

    Basic setup

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+(function() {
+
+  function config($locationProvider, $stateProvider) {
+    $locationProvider
+      .html5Mode({
+        enabled: true, //disables hashbang URLs
+        requireBase: false
+      });
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        controller: 'HomeCtrl as home',
+        templateUrl: '/templates/home.html'
+    });
+  }
+
+  angular
+    .module('pomodoroManager', ['ui.router', 'firebase'])
+    .config(config);
+})();
