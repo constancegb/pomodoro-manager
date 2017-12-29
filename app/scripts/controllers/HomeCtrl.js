299018a55b80ec272eba1d5ae8b7bdc09f326dc2
Constance Gobert <constancegobert@hotmail.fr>
2017-12-29 17:06:18 +0100

    ch 3 completed

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -9,10 +9,10 @@
     // };
 
     $ctrl.time = 1500;
-    $ctrl.startOrResetWork = "START";
-    $ctrl.startOrResetBreak = "START";
+    $ctrl.startOrResetWork = "START WORK";
+    $ctrl.startOrResetBreak = "START BREAK";
 
-    var onBreak = false;
+    $ctrl.onBreak = false;
 
     $ctrl.stopTimer = function() {
       $interval.cancel(promise);
@@ -20,20 +20,38 @@
 
     $ctrl.startTimer = function() {
       //promise = $interval($ctrl.countdown(), 1000, [$ctrl.time]);
-      promise = $interval(function() {$ctrl.time--;}, 1000, [$ctrl.time]);
+      promise = $interval(function() {$ctrl.time--;}, 1000, [$ctrl.time+1]);
     };
 
     $ctrl.workTimer = function() {
-      ($ctrl.startOrResetWork === "START") ? $ctrl.startOrResetWork = "RESET" : $ctrl.startOrResetWork = "START";
-      if ($ctrl.startOrResetWork === "START") {
+      ($ctrl.startOrResetWork === "START WORK") ? $ctrl.startOrResetWork = "RESET" : $ctrl.startOrResetWork = "START WORK";
+      if ($ctrl.startOrResetWork === "START WORK") {
         $ctrl.stopTimer();
         $ctrl.time = 1500;
       } else if ($ctrl.startOrResetWork === "RESET") {
         $ctrl.startTimer();
+        promise.then(function() {
+                      $ctrl.onBreak = true;
+                      $ctrl.time = 300;
+                      $ctrl.startOrResetBreak = "START BREAK";
+                    });
       }
     };
 
-    $ctrl.breakTimer = function() {};
+    $ctrl.breakTimer = function() {
+      ($ctrl.startOrResetBreak === "START BREAK") ? $ctrl.startOrResetBreak = "RESET" : $ctrl.startOrResetBreak = "START BREAK";
+      if ($ctrl.startOrResetBreak === "START BREAK") {
+        $ctrl.stopTimer();
+        $ctrl.time = 300;
+      } else if ($ctrl.startOrResetBreak === "RESET") {
+        $ctrl.startTimer();
+        promise.then(function() {
+                      $ctrl.onBreak = false;
+                      $ctrl.time = 1500;
+                      $ctrl.startOrResetWork = "START WORK";
+                    });
+      }
+    };
 
   }
 
